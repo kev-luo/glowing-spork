310f7f1145480049e89e8efb3fbf6de31e88a142
Kevin Luo <kvn.luo@gmail.com>
2021-07-10 13:57:25 -0500

    nav logo component

---
+++ components/NavigationBar/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Logo() {
+  return (
+    <Box {...props}>
+      <Text fontSize="lg" fontWeight="bold">
+        Logo
+      </Text>
+    </Box>
+  );
+}
