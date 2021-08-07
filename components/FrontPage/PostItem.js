ecd43eebfa4a3ca7e0f3e72af85475a46b6fbc41
Kevin Luo <kvn.luo@gmail.com>
2021-08-07 15:22:40 -0500

    display dates for post

--- components/FrontPage/PostItem.js
+++ components/FrontPage/PostItem.js
@@ -1,7 +1,33 @@
 import PostImage from "./PostImage";
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 export default function PostItem(props) {
   const { post } = props;
+  const returnDate = (postDate) => {
+    const date = new Date(postDate);
+    const year = date.getFullYear();
+    const month = months[date.getMonth()];
+    let dt = date.getDate();
+
+    if (dt < 10) {
+      dt = "0" + dt;
+    }
+    return `${dt} ${month} ${year}`;
+  };
   return (
     <a
       href={`/posts/${post.id}`}
@@ -9,7 +35,9 @@ export default function PostItem(props) {
     >
       <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
         <PostImage label={post.postEmojiTitle} emoji={post.postEmoji} />
-        <span className="mt- text-gray-500 text-sm">12 Jun 2019</span>
+        <span className="mt- text-gray-500 text-sm">
+          {returnDate(post.createdAt)}
+        </span>
       </div>
       <div className="md:flex-grow">
         <h2 className="text-2xl font-medium text-gray-900 title-font mb-1">
